a1daff58aff7c33ebbc29b2fda6b3ca1d67de640
Jonathan Tsai <jontsai@rowhandring-lm.corp.yahoo.com>
2011-06-07 17:27:45 -0700

    adding other changes left out

--- skin/new/standard_header.php
+++ skin/new/standard_header.php
@@ -1,5 +1,5 @@
 <div style="margin: auto; text-align: center;">
-<span style="font-family:Courier New,serif; font-weight: bold; font-size:72px; letter-spacing:10px;">jontsai</span>
+<a href="http://jontsai.xanga.com" style="text-decoration: none; font-family:Courier New,serif; font-weight: bold; font-size:72px; letter-spacing:10px; color: white;">jontsai</a>
 <br/>
 <span style="font-style: italic; font-family:Palatino Linotype,Palatino,serif;">"Some quote here"</span>
 <br/>
