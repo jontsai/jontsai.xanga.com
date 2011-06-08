ddc5518b5b73a211aeca24616178ac6d06191971
Jonathan Tsai <akajontsai-devel@yahoo.com>
2011-06-08 00:35:57 -0700

    update links

--- skin/new/links.php
+++ skin/new/links.php
@@ -1,4 +1,5 @@
 <a href="http://jontsai.xanga.com" title="Home">Home</a>
+| <a href="http://about.me/jontsai" target="_blank">About Me</a>
 | <a href="http://www.aweandreverence.com/" title="Bible Reflections" target="_blank">Awe and Reverence</a>
 | <a href="http://jontsai.xanga.com/672319336/affiliate-store/"  title="Affiliate Store">Store</a>
 | <a href="http://jontsai.xanga.com/727050472/donate/" title="Make Donations">Donate</a>
--- skin/new/social_411.php
+++ skin/new/social_411.php
@@ -1,8 +1,7 @@
 <ul style="list-style-type: none; margin: 0px; padding: 0px;">
-<li><a href="http://about.me/jontsai" target="_blank">About Me</a></li>
-<li><a href="http://www.linkedin.com/in/jontsai" target="_blank">LinkedIn</a></li>
 <li><a href="http://www.flickr.com/jontsai8601" target="_blank">Flickr</a></li>
 <li><a href="http://www.twitter.com/jontsai" target="_blank">Twitter</a></li>
+<li><a href="http://www.linkedin.com/in/jontsai" target="_blank">LinkedIn</a></li>
 <li><a href="http://www.facebook.com/jontsai" target="_blank">Facebook</a></li>
 <li><a href="http://www.google.com/profiles/jontsai" target="_blank">Google</a></li>
 <li><a href="http://jontsai.myplaxo.com/" target="_blank">Plaxo</a></li>
